9745fbdb91845d6d59752a2361904eb9c4e762ad
franciscofornicola <franciscofornicola@gmail.com>
2025-06-06 14:14:19 -0300

    Remove uvicorn do build.sh para usar Start Command no Render

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 pip install --upgrade pip
 pip install sqlalchemy==2.0.28
-pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port $PORT 
+pip install -r requirements.txt 
