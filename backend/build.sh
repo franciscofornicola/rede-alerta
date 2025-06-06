ad182d297477ede19fd9f9faa0387add4462c5e6
franciscofornicola <franciscofornicola@gmail.com>
2025-06-06 14:11:11 -0300

    Build e start combinados no build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install --upgrade pip
 pip install sqlalchemy==2.0.28
-pip install -r requirements.txt 
+pip install -r requirements.txt
+uvicorn main:app --host 0.0.0.0 --port $PORT 
